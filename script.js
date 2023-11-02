12d81e2675276a866352dc8dc1cc43061321e65b
Akimin Oleg <akimin97@mail.ru>
2023-11-02 23:46:49 +0300

    start mobile

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const swiperGoods = new Swiper('.swiperGoods', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 4,
-    spaceBetween: 18,
-
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-  
+document.addEventListener("DOMContentLoaded", () => {
+    const swiperGoods = new Swiper('.swiperGoods', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 18,
+    
+    
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+      
+    });
 });
